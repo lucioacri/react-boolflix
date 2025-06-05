b7b6ccf8f35500437fd5e778086006945905def0
lucioacri <lucioacri@gmail.com>
2025-06-05 12:10:49 +0200

    add results list

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,38 @@
+import { useState, useEffect } from "react";
+import axios from "axios";
+
 function App() {
-  return <></>;
+  const apiKey = import.meta.env.VITE_API_KEY;
+  const apiUrl = "https://api.themoviedb.org/3/search/movie";
+  const apiParams = {
+    api_key: apiKey,
+    query: "ritorno",
+  };
+
+  const queryString = new URLSearchParams(apiParams).toString();
+
+  const [resultsList, setResultsList] = useState([]);
+
+  useEffect(() => {
+    axios
+      .get(`${apiUrl}?${queryString}`)
+      .then((res) => setResultsList(res.data.results));
+  }, []);
+  return (
+    <>
+      <div className="container">
+        <div className="row">
+          <div className="col">
+            <ul>
+              {resultsList.map((result, index) => (
+                <li key={index}>{result.title}</li>
+              ))}
+            </ul>
+          </div>
+        </div>
+      </div>
+    </>
+  );
 }
 
 export default App;
